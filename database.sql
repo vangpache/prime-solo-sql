5218e35e9b21c0690f057fea99ea5058d893c967
vangpache <44348828+vangpache@users.noreply.github.com>
2019-08-06 14:40:18 -0500

    UPDATE ANSWER 2;

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ WHERE "username" ILIKE '%a%';
 -- 2. The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
 UPDATE "accounts"
 SET "account_balance" = 10.00
-WHERE "transactions_attempted" = 0;
+WHERE "transactions_attempted" = 0 AND "account_balance" = 0;
 
 -- 3. How do you select all users that have attempted 9 or more transactions?
 SELECT "username" FROM "accounts"
@@ -28,4 +28,5 @@ SELECT "username" FROM "accounts"
 WHERE "account_balance" > 100;
 
 -- 7. How do you add a new account?
-The bank is losing money in Miami and Phoenix and needs to unload low transaction custom
+
+-- 8. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
